91fa99a3ed53f62abe5a4df5ad89e53563d1eb3a
Gaurav Chandrashekar <cggaurav@gmail.com>
2013-06-14 16:25:43 +0200

    8ts coming!

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+require 'meta-spotify'
+require 'json'
+
+set :protection, except: :ip_spoofing
+
+get '/' do
+  erb :index
+end
+
+
+post '/8ts' do
+	username = params[:username]
+	url = "http://8tracks.com/users/" + username + "/favorite_tracks"
+	# 8tracks.com/users/mahesh427/favorite_tracks
+	html = Nokogiri::HTML(open(url))
+	tracks = []
+	html.css('.track_info').each do |i|
+		tracks << i.text
+	end
+	uris = []
+	tracks.each do |track|
+		result = MetaSpotify::Track.search(track)
+		uris << result[:tracks][0].uri.gsub("spotify:track:","") if (result.count > 0)
+	end
+	# https://embed.spotify.com/?uri=spotify:trackset:cggaurav:0Qw7qGuXlVBZLcfoQ17yZn,5tu9ExlYqrlSk1MjAMTr2O,67Hna13dNDkZvBpTXRIaOJ&theme=white&view=coverart
+	src = 'https://embed.spotify.com/?uri=spotify:trackset:'
+	src += username.to_s
+	src += ":"
+	src += uris.join(",")
+	src += '&theme=white&view=coverart'
+	return {'src' => src}.to_json
+end
